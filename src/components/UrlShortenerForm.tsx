d3c79202a8049c463888ae613822b3c398a4a716
NicKyeaa <tin.jurcevic@gmail.com>
2023-09-03 21:11:48 +0200

    Added axios post

--- src/components/UrlShortenerForm.tsx
+++ src/components/UrlShortenerForm.tsx
@@ -1,25 +1,28 @@
-import React from "react";
-import { Input, Form, Space, Button } from "antd";
+import React from 'react';
+import axios from 'axios';
+import { Input, Form, Space, Button } from 'antd';
 
-const onFinish = (values: unknown) => {
-  console.log("Success", values);
+const onFinish = async (values: unknown) => {
+  console.log('Success', values);
+  const result = axios.post('http://localhost:3000/shorten', values);
+  console.log(result);
 };
 
 function UrlShortenerForm() {
   return (
     <>
       <Form name='SubmitUrl' onFinish={onFinish}>
-        <Space.Compact style={{ width: "100%" }}>
+        <Space.Compact style={{ width: '100%' }}>
           <Form.Item
             name='URL'
             rules={[
               {
                 required: true,
-                message: "Please input your URL",
+                message: 'Please input your URL',
               },
               {
-                type: "url",
-                message: "Please enter a valid URL",
+                type: 'url',
+                message: 'Please enter a valid URL',
               },
             ]}
           >
