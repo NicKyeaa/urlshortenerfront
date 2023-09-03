22ecfbae574d13c3608eb752f1de0b655e21da33
NicKyeaa <tin.jurcevic@gmail.com>
2023-09-03 22:46:44 +0200

    Removed react library

--- src/components/UrlShortenerForm.tsx
+++ src/components/UrlShortenerForm.tsx
@@ -1,14 +1,12 @@
-import React from 'react';
 import axios from 'axios';
 import { Input, Form, Space, Button } from 'antd';
 
 const onFinish = async (values: unknown) => {
   // Making a POST request to shorten an URL
-
   try {
     console.log('Success', values);
 
-    const result = axios.post('http://localhost:3000/shorten', values);
+    const result = await axios.post('http://localhost:3000/shorten', values);
     console.log(result);
   } catch (error) {
     console.error(error);
